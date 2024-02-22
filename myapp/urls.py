f56f416bb67a2f4e7942a92960a550bd0d2e4044
magertwigs <morganahidi@gmail.com>
2024-02-22 13:54:49 +0300

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,1 @@
+from django.contrib import adminfrom django.urls import pathfrom myapp import viewsurlpatterns = [    path('admin/', admin.site.urls),    path('', views.home ),    path('gallery/', views.images ),    path('about/', views.about ),    path('contact/', views.contact ),    path('collection/', views.collection ),]
